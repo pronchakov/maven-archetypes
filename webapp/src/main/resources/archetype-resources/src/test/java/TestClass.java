77a213760cfd219d07e4aed51f6f6189266bd3e3
Artem Pronchakov <artem.pronchakov@calisto.email>
2015-02-28 19:28:02 +0300

    Add logging and testing to WebApp Archetype

---
+++ webapp/src/main/resources/archetype-resources/src/test/java/TestClass.java
@@ -0,0 +1,19 @@
+package $package;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.junit.Assert.*;
+
+public class TestClass {
+
+    private static final Logger log = LoggerFactory.getLogger(TestClass.class);
+
+    @Test
+    @Ignore
+    public void test() {
+
+    }
+
+}
