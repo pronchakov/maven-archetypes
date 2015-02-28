eb59bf65c531ed6e94337348490cb30cb3cdf3a9
Artem Pronchakov <artem.pronchakov@calisto.email>
2015-02-28 19:11:11 +0300

    Add manifest entries and logging

--- runnable-jar/src/main/resources/archetype-resources/src/main/java/Main.java
+++ runnable-jar/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -1,6 +1,11 @@
 package $package;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
